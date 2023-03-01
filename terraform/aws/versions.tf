96875cb1f7d9804bd15f5a250135d23076ed45f6
jontie-wakeham <jontie@MacBook-Air.local>
2023-03-01 14:43:54 +1300

    remove gcp and added the correct versions for the providers

--- terraform/aws/versions.tf
+++ terraform/aws/versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 0.13.1"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.73"
+    terraform-provider-aws = {
+      source  = "hashicorp/terraform-provider-aws"
+      version = ">= 4.56.0"
     }
   }
 }
--- terraform/azure/versions.tf
+++ terraform/azure/versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 0.13.1"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.73"
+    terraform-provider-azurerm = {
+      source  = "hashicorp/terraform-provider-azurerm"
+      version = ">= 3.45.0"
     }
   }
 }
